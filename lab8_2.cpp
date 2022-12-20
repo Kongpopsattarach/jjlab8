743fdde0fb660814fc12b7fc20dd65047c2623e8
JJ <kongpopsattarach.edu@gmail.com>
2022-12-20 16:35:53 +0700

    update lab 8 2

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -1,4 +1,50 @@
-//Dialog of the first example is given below.
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int ID;
+    string name,movie,date,text;
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?\n";
+    cout << "?????: ";
+    getline(cin,name);
+    cout << "Fahsai: Wow!!! " << name <<  " is a really cool name.\n";
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID?\n";
+    cout << name << ":";
+    cin >> ID;
+    cin.ignore();
+    cout << "Fahsai: I think you may be GEAR 47. I have a free movie ticket for you\n";
+    cout << "Fahsai: Let's go to the cinema together!!!\n";
+    cout << "Fahsai: What movie do you want to watch?\n";
+    cout << name << ":";
+    getline(cin,movie);
+    cout << "Fahsai: So....which day are you free to go with me?\n";
+    cout << name << ": ";
+    getline(cin,date);
+    cout << "Fahsai: "<< date <<"....that is OK!!! I'm looking forward to watching " << movie << " with you.\n";
+    cout << name << ": ";
+    getline(cin,text);
+    cout << "Fahsai: 555+ see you Friday the 13th. Bye Bye \\(^ ^)/";
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 Fahsai: Sawadee ka...Can you tell me your name?
